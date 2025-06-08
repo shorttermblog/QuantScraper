a24723d7679e9e68ff44de5e90bdb61cc77d3243
shorttermblog <117600915+shorttermblog@users.noreply.github.com>
2025-06-08 22:50:29 +0200

    Update backtestapp6.py

--- backtestapp6.py
+++ backtestapp6.py
@@ -242,7 +242,7 @@ def execute_strategy(df, strategy, params):
 
         df['Position'] = df['Signal'].diff().fillna(0)
 
-    elif strategy == "MinN Close":
+    elif strategy == "Min last n days":
         st.markdown(
             "<h2 style='font-size:20px;'>The strategy buys when the closing price is the minimum of the last n days</h2>",
             unsafe_allow_html=True)
@@ -289,7 +289,7 @@ with st.sidebar.expander("Data Settings", expanded=True):
 
 with st.sidebar.expander("Strategy Parameters", expanded=True):
     strategy = st.selectbox("Select Strategy",
-                            options=["Moving Average Crossover", "Momentum", "RSI_MA", "Streak", "MinN Close"])
+                            options=["Moving Average Crossover", "Momentum", "RSI_MA", "Streak", "Min last n days"])
     strategy_params = {}
     if strategy == "Moving Average Crossover":
         st.markdown("#### Moving Average Parameters")
@@ -317,7 +317,7 @@ with st.sidebar.expander("Strategy Parameters", expanded=True):
         )
         strategy_params["holding_period"] = st.slider("Holding Period (days)", min_value=1, max_value=10, value=3,
                                                       step=1)
-    elif strategy == "MinN Close":
+    elif strategy == "Min last n days":
         st.markdown("#### MinN Strategy Parameters")
         strategy_params["lookback"] = st.slider("Lookback Period (n days)", min_value=2, max_value=100, value=5, step=1)
         strategy_params["holding_period"] = st.slider("Holding Period (y days)", min_value=1, max_value=10, value=3, step=1)
